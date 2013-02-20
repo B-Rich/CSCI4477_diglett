64afdb7c1b333ea6919aeb02c3e4d6f7138bba9f
= <=>
2013-02-20 04:36:29 -0400

    Finished async shipping of csv data from cross origin to client.

--- js/dawson.js
+++ js/dawson.js
@@ -6,14 +6,20 @@
 var socket = io.connect('http://localhost:8080');
 
 function updateData(keyValue) {
+    
+    logger.log('pull data');
+    
     socket.emit('pull data', { key : keyValue });
     socket.on('push data', function (data) {
-        console.log(JSON.stringify(data));
+        
+        logger.log('push recieved');
+        dataSet = data;
     });
 } 
 
 function test_updateData() {
     
     updateData('randat');
+    console.log(dataSet);
 }
 test_updateData();
