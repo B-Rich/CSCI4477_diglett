28a73f721b726f5ad09ddf1d3eb5c2aed10e88fd
= <=>
2013-02-21 16:51:39 -0400

    Added upload dialog and main js file.

--- js/dawson.js
+++ js/dawson.js
@@ -39,3 +39,28 @@ function test_uploadFile() {
     uploadFile();
 }
 //test_uploadFile();
+
+/*
+ * Sets up the custom upload file dialog.
+ */
+function setupUploadFile() {
+     
+    $('#dm-file-upload').css({
+        left : ($(window).width() - $('#dm-file-upload').width()) / 2,
+        top : ($(window).height() - $('#dm-file-upload').height()) / 2
+    });
+    
+    $(document).click(function() {
+        $('#dm-file-upload').css({ visibility : 'hidden' });
+    });
+    
+    $("#dm-file-upload").click(function(e) {
+        e.stopPropagation(); 
+    });
+    
+    $("#upload-button").click(function(e) {
+        e.stopPropagation(); 
+        $('#dm-file-upload').css({ visibility : 'visible' });
+        $('html, body').animate({scrollTop:0}, 'slow');
+    });
+}
